16c8d12591bc0fd057e68fe7b590899109422799
Hugo Rodrigues <hugo.rodrigues@hiperformancesales.com>
2016-11-04 08:44:27 +0000

    missing test to access items process directtly

--- controllers/shoppingCart/process.js
+++ controllers/shoppingCart/process.js
@@ -40,27 +40,29 @@ process.prototype.postRoute = function(){
   };
 };
 
-process.prototype.processItems = function(){
+process.prototype.processItems = function(order){
   let _this = this;
   return new Promise ((resolve, reject) => {
+    // external access save new order
+    if(!order) order = _this.cart;
 
     // set up properties for backup before applying filters
-    _this.originalCart=Object.assign({}, _this.cart);
+    _this.originalCart=Object.assign({}, order);
 
     let applyFilters = [];
-    let newCart = _this.cart;
+    let newCart = order;
 
     // apply all filters in folder to promise array
     filters.map((runFilter) => {
-      // no need to create new object Object.assign({},_this.cart)
+      // no need to create new object Object.assign({},order)
       applyFilters.push(runFilter.processItems(newCart));
     });
 
     // run promises
     Promise.all(applyFilters).then((results) => {
       //process results
-      //_this.cart.items = result.items;
-      //_this.cart.total = result.total;
+      //order.items = result.items;
+      //order.total = result.total;
       resolve(newCart);
     });
 
--- test/shoppingCart.js
+++ test/shoppingCart.js
@@ -2,6 +2,8 @@
 
 const chai = require('chai');
 const server = require('../');
+const processCart = require('../controllers/shoppingCart/process');
+const goodCustomer = require('../controllers/filters/goodCustomer');
 
 // Using Expect style
 let expect = chai.expect;
@@ -49,6 +51,43 @@ describe('order process', () => {
       expect(response.statusCode).to.equal(400);
       done();
     });
+  });
+
+  it('can use process items function directly', (done) => {
+    let order = {
+      "id": "1",
+      "customer-id": "2",
+      "items": [
+        {
+          "product-id": "B102",
+          "quantity": "10",
+          "unit-price": "4.99",
+          "total": "49.90"
+        },
+        {
+          "product-id": "A102",
+          "quantity": "1",
+          "unit-price": "49.50",
+          "total": "49.50"
+        }
+      ],
+      "total": "99.4"
+    };
+
+    processCart.processItems(order).then((result) => {
+      let found = false;
+
+      result.items[0].discount.map((itemDiscount)=>{
+        if(itemDiscount.name==goodCustomer.discountName)found=true;
+      })
+
+      expect(found).to.be.true;
+      expect(result.total).to.equal((result.items[0].total + result.items[1].total))
+      done();
+
+
+    }).catch(err => console.log(err))
+
   });
 
 });
